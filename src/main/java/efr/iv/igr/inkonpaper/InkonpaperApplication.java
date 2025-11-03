68e1ca856ee75943ea4285f6a821bedee9013426
Ivan <iamivanefremenkov@gmail.com>
2025-11-03 10:03:35 +0300

    chore(init): init project

---
+++ src/main/java/efr/iv/igr/inkonpaper/InkonpaperApplication.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.inkonpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkonpaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkonpaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/efr/iv/igr/inkonpaper/InkonpaperApplicationTests.java
@@ -0,0 +1,13 @@
+package efr.iv.igr.inkonpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkonpaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

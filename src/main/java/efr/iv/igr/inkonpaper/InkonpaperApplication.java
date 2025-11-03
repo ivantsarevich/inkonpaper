89eec0b7c554dce4abdf0ff856a76f9e07fb0a33
Ivan <iamivanefremenkov@gmail.com>
2025-11-03 10:21:13 +0300

    feat(db): integrate database with application

--- src/main/java/efr/iv/igr/inkonpaper/InkonpaperApplication.java
+++ src/main/java/efr/iv/igr/inkonpaper/InkonpaperApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InkonpaperApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InkonpaperApplication.class, args);
     }
-
 }
